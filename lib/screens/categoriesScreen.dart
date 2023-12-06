466d4bf423568bcd8b95eab61c933285810dddc7
Prabhat8991 <singh.prabhat8991@gmail.com>
2023-12-06 15:49:19 +0530

    Added Category screen and Meals screen

---
+++ lib/screens/categoriesScreen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals_screen.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+
+import '../models/category.dart';
+import '../models/meal.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  void _onCategorySelected(BuildContext context, Category category) {
+    List<Meal> filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (ctx) =>
+                MealsScreen(title: category.title, meals: filteredMeals)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pick a category"),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20,
+            childAspectRatio: 3 / 2),
+        children: availableCategories
+            .map((category) => CategoryGridItem(
+                  category: category,
+                  onCategorySelected: () {
+                    _onCategorySelected(context, category);
+                  },
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/meals_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import '../models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget? body;
+
+    if (meals.isEmpty) {
+      body = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "Oh oh.... nothing here",
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge
+                  ?.copyWith(color: Theme.of(context).colorScheme.onBackground),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text("Please select another category",
+                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground)),
+          ],
+        ),
+      );
+    } else {
+      body = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (context, index) => Column(
+          children: [
+            Text(meals[index].title),
+            Text(meals[index].imageUrl),
+          ],
+        ),
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: body,
+    );
+  }
+}
